3e16b0f791eafcd25974e4952093f19d6e1db00e
Willian <willian.alves.b15@gmail.com>
2022-07-29 18:18:55 -0300

    feat: created MongoModel

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,37 @@
+import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model:Model<T>;
+
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj:T):Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async readOne(_id:string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async read(): Promise<T[]> {
+    return this._model.find({});
+  }
+
+  public async update(param: string, obj: T): Promise<T | null> {
+    return this._model.findByIdAndUpdate(
+      { param },
+      { ...obj } as UpdateQuery<T>,
+      { new: true },
+    );
+  }
+
+  public async delete(param: string, obj: T): Promise<T | null> {
+    return this._model.findByIdAndDelete({ param }, { obj });
+  }
+}
+
+export default MongoModel;
