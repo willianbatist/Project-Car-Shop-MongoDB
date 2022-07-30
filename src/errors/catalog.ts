65ff923e66bf18cecaee9ee92da0acc2c7bde4e1
Willian <willian.alves.b15@gmail.com>
2022-07-29 21:41:06 -0300

    fix: created errorCatalog

--- src/errors/catalog.ts
+++ src/errors/catalog.ts
@@ -3,4 +3,28 @@ export enum ErrorTypes {
   InvalidMongoId = 'InvalidMongoId',
 }
 
+type ErrorResponseObject = { 
+  message: string;
+  httpStatus: number
+};
+
+// aqui o tipo do catálogo
+export type ErrorCatalog = {
+  // onde cada chave desse objeto é uma chave do Enum ErrorTypes
+  // e cada valor é um objeto de resposta da API
+  [key in ErrorTypes]: ErrorResponseObject
+
+};
+
+export const errorCatalog: ErrorCatalog = {
+  EntityNotFound: {
+    message: 'Entity not found',
+    httpStatus: 404,
+  },
+  InvalidMongoId: {
+    message: 'Id must be a 24 characters hexadecimal',
+    httpStatus: 400,
+  },
+};
+
 export const wi = {};
