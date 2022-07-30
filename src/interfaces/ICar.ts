f991634a60f6d8674b2f863caf185e33fac55732
Willian <willian.alves.b15@gmail.com>
2022-07-30 18:18:57 -0300

    fix: ICar added min 2 in doorsQty and seatsQty

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
 export const CarZodSchema = VehicleZodSchema.extend({
-  doorsQty: z.number(),
-  seatsQty: z.number(),
+  doorsQty: z.number().min(2),
+  seatsQty: z.number().min(2),
 });
 
 export type ICar = z.infer<typeof CarZodSchema>;
